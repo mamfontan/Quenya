4e59f988ada796e55aa0020976dcc9b36c5c64b5
Miguel Angel Moreno Fontán <mamfontan@gmail.com>
2020-12-06 20:09:19 +0100

    New functionality for Stock Detail

--- Quenya.View/FrmStockDetail.Designer.cs
+++ Quenya.View/FrmStockDetail.Designer.cs
@@ -28,44 +28,49 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.btnSave = new System.Windows.Forms.Button();
-            this.btnCancel = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.Button();
             this.gbStockValueInfo = new System.Windows.Forms.GroupBox();
             this.lblDescriptionValue = new System.Windows.Forms.Label();
             this.lblAddress = new System.Windows.Forms.Label();
             this.lblAddressValue = new System.Windows.Forms.Label();
             this.btnExport = new System.Windows.Forms.Button();
+            this.lblAssetTypeValue = new System.Windows.Forms.Label();
+            this.lblAssetType = new System.Windows.Forms.Label();
+            this.lblExchangeValue = new System.Windows.Forms.Label();
+            this.lblExchange = new System.Windows.Forms.Label();
+            this.lblCurrencyValue = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblCountryValue = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lblSectorValue = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.lblIndustryValue = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.lblEmployeesValue = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
             this.gbStockValueInfo.SuspendLayout();
             this.SuspendLayout();
             // 
-            // btnSave
+            // btnClose
             // 
-            this.btnSave.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.btnSave.Location = new System.Drawing.Point(655, 460);
-            this.btnSave.Name = "btnSave";
-            this.btnSave.Size = new System.Drawing.Size(94, 29);
-            this.btnSave.TabIndex = 0;
-            this.btnSave.Text = "Save";
-            this.btnSave.UseVisualStyleBackColor = true;
-            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
-            // 
-            // btnCancel
-            // 
-            this.btnCancel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
-            this.btnCancel.Location = new System.Drawing.Point(12, 460);
-            this.btnCancel.Name = "btnCancel";
-            this.btnCancel.Size = new System.Drawing.Size(94, 29);
-            this.btnCancel.TabIndex = 0;
-            this.btnCancel.Text = "Cancel";
-            this.btnCancel.UseVisualStyleBackColor = true;
-            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            this.btnClose.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnClose.Location = new System.Drawing.Point(573, 368);
+            this.btnClose.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(82, 22);
+            this.btnClose.TabIndex = 0;
+            this.btnClose.Text = "Close";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
             // 
             // gbStockValueInfo
             // 
             this.gbStockValueInfo.Controls.Add(this.lblDescriptionValue);
-            this.gbStockValueInfo.Location = new System.Drawing.Point(260, 12);
+            this.gbStockValueInfo.Location = new System.Drawing.Point(228, 9);
+            this.gbStockValueInfo.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.gbStockValueInfo.Name = "gbStockValueInfo";
-            this.gbStockValueInfo.Size = new System.Drawing.Size(489, 400);
+            this.gbStockValueInfo.Padding = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.gbStockValueInfo.Size = new System.Drawing.Size(428, 324);
             this.gbStockValueInfo.TabIndex = 1;
             this.gbStockValueInfo.TabStop = false;
             this.gbStockValueInfo.Text = " Stock value name  ";
@@ -74,18 +79,18 @@
             // 
             this.lblDescriptionValue.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
             this.lblDescriptionValue.ForeColor = System.Drawing.Color.Blue;
-            this.lblDescriptionValue.Location = new System.Drawing.Point(18, 34);
+            this.lblDescriptionValue.Location = new System.Drawing.Point(16, 22);
             this.lblDescriptionValue.Name = "lblDescriptionValue";
-            this.lblDescriptionValue.Size = new System.Drawing.Size(454, 350);
+            this.lblDescriptionValue.Size = new System.Drawing.Size(397, 291);
             this.lblDescriptionValue.TabIndex = 2;
-            this.lblDescriptionValue.Text = "Address";
+            this.lblDescriptionValue.Text = "Description";
             // 
             // lblAddress
             // 
             this.lblAddress.AutoSize = true;
-            this.lblAddress.Location = new System.Drawing.Point(23, 394);
+            this.lblAddress.Location = new System.Drawing.Point(20, 318);
             this.lblAddress.Name = "lblAddress";
-            this.lblAddress.Size = new System.Drawing.Size(65, 20);
+            this.lblAddress.Size = new System.Drawing.Size(52, 15);
             this.lblAddress.TabIndex = 2;
             this.lblAddress.Text = "Address:";
             // 
@@ -93,35 +98,183 @@
             // 
             this.lblAddressValue.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
             this.lblAddressValue.ForeColor = System.Drawing.Color.Blue;
-            this.lblAddressValue.Location = new System.Drawing.Point(23, 423);
+            this.lblAddressValue.Location = new System.Drawing.Point(20, 339);
             this.lblAddressValue.Name = "lblAddressValue";
-            this.lblAddressValue.Size = new System.Drawing.Size(726, 20);
+            this.lblAddressValue.Size = new System.Drawing.Size(635, 15);
             this.lblAddressValue.TabIndex = 2;
             this.lblAddressValue.Text = "Address";
             // 
             // btnExport
             // 
             this.btnExport.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.btnExport.Location = new System.Drawing.Point(543, 460);
+            this.btnExport.Location = new System.Drawing.Point(475, 368);
+            this.btnExport.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btnExport.Name = "btnExport";
-            this.btnExport.Size = new System.Drawing.Size(94, 29);
+            this.btnExport.Size = new System.Drawing.Size(82, 22);
             this.btnExport.TabIndex = 0;
             this.btnExport.Text = "Export";
             this.btnExport.UseVisualStyleBackColor = true;
             this.btnExport.Click += new System.EventHandler(this.btnExport_Click);
             // 
+            // lblAssetTypeValue
+            // 
+            this.lblAssetTypeValue.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lblAssetTypeValue.ForeColor = System.Drawing.Color.Blue;
+            this.lblAssetTypeValue.Location = new System.Drawing.Point(23, 35);
+            this.lblAssetTypeValue.Name = "lblAssetTypeValue";
+            this.lblAssetTypeValue.Size = new System.Drawing.Size(182, 15);
+            this.lblAssetTypeValue.TabIndex = 3;
+            this.lblAssetTypeValue.Text = "Asset type";
+            // 
+            // lblAssetType
+            // 
+            this.lblAssetType.AutoSize = true;
+            this.lblAssetType.Location = new System.Drawing.Point(23, 19);
+            this.lblAssetType.Name = "lblAssetType";
+            this.lblAssetType.Size = new System.Drawing.Size(64, 15);
+            this.lblAssetType.TabIndex = 4;
+            this.lblAssetType.Text = "Asset type:";
+            // 
+            // lblExchangeValue
+            // 
+            this.lblExchangeValue.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lblExchangeValue.ForeColor = System.Drawing.Color.Blue;
+            this.lblExchangeValue.Location = new System.Drawing.Point(23, 76);
+            this.lblExchangeValue.Name = "lblExchangeValue";
+            this.lblExchangeValue.Size = new System.Drawing.Size(182, 15);
+            this.lblExchangeValue.TabIndex = 5;
+            this.lblExchangeValue.Text = "Exchange";
+            // 
+            // lblExchange
+            // 
+            this.lblExchange.AutoSize = true;
+            this.lblExchange.Location = new System.Drawing.Point(23, 60);
+            this.lblExchange.Name = "lblExchange";
+            this.lblExchange.Size = new System.Drawing.Size(61, 15);
+            this.lblExchange.TabIndex = 6;
+            this.lblExchange.Text = "Exchange:";
+            // 
+            // lblCurrencyValue
+            // 
+            this.lblCurrencyValue.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lblCurrencyValue.ForeColor = System.Drawing.Color.Blue;
+            this.lblCurrencyValue.Location = new System.Drawing.Point(23, 118);
+            this.lblCurrencyValue.Name = "lblCurrencyValue";
+            this.lblCurrencyValue.Size = new System.Drawing.Size(182, 15);
+            this.lblCurrencyValue.TabIndex = 7;
+            this.lblCurrencyValue.Text = "Currency";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(23, 102);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 15);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Currency:";
+            // 
+            // lblCountryValue
+            // 
+            this.lblCountryValue.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lblCountryValue.ForeColor = System.Drawing.Color.Blue;
+            this.lblCountryValue.Location = new System.Drawing.Point(23, 160);
+            this.lblCountryValue.Name = "lblCountryValue";
+            this.lblCountryValue.Size = new System.Drawing.Size(182, 15);
+            this.lblCountryValue.TabIndex = 9;
+            this.lblCountryValue.Text = "Country";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(23, 144);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(53, 15);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Country:";
+            // 
+            // lblSectorValue
+            // 
+            this.lblSectorValue.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lblSectorValue.ForeColor = System.Drawing.Color.Blue;
+            this.lblSectorValue.Location = new System.Drawing.Point(23, 203);
+            this.lblSectorValue.Name = "lblSectorValue";
+            this.lblSectorValue.Size = new System.Drawing.Size(182, 15);
+            this.lblSectorValue.TabIndex = 11;
+            this.lblSectorValue.Text = "Sector";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(23, 187);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(43, 15);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "Sector:";
+            // 
+            // lblIndustryValue
+            // 
+            this.lblIndustryValue.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lblIndustryValue.ForeColor = System.Drawing.Color.Blue;
+            this.lblIndustryValue.Location = new System.Drawing.Point(23, 248);
+            this.lblIndustryValue.Name = "lblIndustryValue";
+            this.lblIndustryValue.Size = new System.Drawing.Size(182, 15);
+            this.lblIndustryValue.TabIndex = 13;
+            this.lblIndustryValue.Text = "Industry";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(23, 232);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(53, 15);
+            this.label8.TabIndex = 14;
+            this.label8.Text = "Industry:";
+            // 
+            // lblEmployeesValue
+            // 
+            this.lblEmployeesValue.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lblEmployeesValue.ForeColor = System.Drawing.Color.Blue;
+            this.lblEmployeesValue.Location = new System.Drawing.Point(23, 290);
+            this.lblEmployeesValue.Name = "lblEmployeesValue";
+            this.lblEmployeesValue.Size = new System.Drawing.Size(182, 15);
+            this.lblEmployeesValue.TabIndex = 15;
+            this.lblEmployeesValue.Text = "Employess";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(23, 273);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(67, 15);
+            this.label10.TabIndex = 16;
+            this.label10.Text = "Employees:";
+            // 
             // FrmStockDetail
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(761, 501);
+            this.ClientSize = new System.Drawing.Size(666, 399);
+            this.Controls.Add(this.lblEmployeesValue);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.lblIndustryValue);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.lblSectorValue);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.lblCountryValue);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.lblCurrencyValue);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lblExchangeValue);
+            this.Controls.Add(this.lblExchange);
+            this.Controls.Add(this.lblAssetTypeValue);
+            this.Controls.Add(this.lblAssetType);
             this.Controls.Add(this.btnExport);
             this.Controls.Add(this.lblAddressValue);
             this.Controls.Add(this.lblAddress);
             this.Controls.Add(this.gbStockValueInfo);
-            this.Controls.Add(this.btnCancel);
-            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnClose);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "FrmStockDetail";
@@ -135,12 +288,25 @@
 
         #endregion
 
-        private System.Windows.Forms.Button btnSave;
-        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.Button btnClose;
         private System.Windows.Forms.GroupBox gbStockValueInfo;
         private System.Windows.Forms.Label lblAddress;
         private System.Windows.Forms.Label lblAddressValue;
         private System.Windows.Forms.Label lblDescriptionValue;
         private System.Windows.Forms.Button btnExport;
+        private System.Windows.Forms.Label lblAssetTypeValue;
+        private System.Windows.Forms.Label lblAssetType;
+        private System.Windows.Forms.Label lblExchangeValue;
+        private System.Windows.Forms.Label lblExchange;
+        private System.Windows.Forms.Label lblCurrencyValue;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblCountryValue;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lblSectorValue;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label lblIndustryValue;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label lblEmployeesValue;
+        private System.Windows.Forms.Label label10;
     }
 }
--- Quenya.View/FrmStockDetail.cs
+++ Quenya.View/FrmStockDetail.cs
@@ -1,4 +1,5 @@
 ﻿using Quenya.Common.interfaces;
+using Quenya.Domain;
 using System;
 using System.Collections.Generic;
 using System.Windows.Forms;
@@ -27,7 +28,7 @@ namespace Quenya.View
 
         private void FrmStockDetail_Load(object sender, EventArgs e)
         {
-            HookButtonEvents(new List<Control>() { btnCancel, btnSave });
+            HookButtonEvents(new List<Control>() { btnExport, btnClose });
 
             LoadOverviewData();
         }
@@ -39,30 +40,59 @@ namespace Quenya.View
 
             var overview = _database.GetStockOverviewByCode(_stockCode);
             if (overview != null)
-            {
-                gbStockValueInfo.Text = " " + overview.Name + "  ";
-                lblDescriptionValue.Text = overview.Description;
-                lblAddressValue.Text = overview.Address;
-            }
+                ShowData(overview);
             else
             {
-                gbStockValueInfo.Text = " No info  ";
-                lblDescriptionValue.Text = string.Empty;
-                lblAddressValue.Text = string.Empty;
+                if (_api != null)
+                {
+                    overview = _api.SearchStockOverview(_stockCode);
+                    if (overview != null)
+                        ShowData(overview);
+                    else
+                        ClearData();
+                }
+                else
+                    ClearData();
             }
         }
 
-        private void btnCancel_Click(object sender, EventArgs e)
+        private void ShowData(Overview data)
         {
-            Close();
+            if (data != null)
+            {
+                gbStockValueInfo.Text = " " + data.Name + "  ";
+                lblDescriptionValue.Text = data.Description;
+                lblAssetTypeValue.Text = data.AssetType;
+                lblExchangeValue.Text = data.Exchange;
+                lblCurrencyValue.Text = data.Currency;
+                lblCountryValue.Text = data.Country;
+                lblSectorValue.Text = data.Sector;
+                lblIndustryValue.Text = data.Industry;
+                lblEmployeesValue.Text = data.FullTimeEmployees;
+                lblAddressValue.Text = data.Address;
+            }
+        }
+
+        private void ClearData()
+        {
+            gbStockValueInfo.Text = " No info  ";
+            lblDescriptionValue.Text = string.Empty;
+            lblAssetTypeValue.Text = string.Empty;
+            lblExchangeValue.Text = string.Empty;
+            lblCurrencyValue.Text = string.Empty;
+            lblCountryValue.Text = string.Empty;
+            lblSectorValue.Text = string.Empty;
+            lblIndustryValue.Text = string.Empty;
+            lblEmployeesValue.Text = string.Empty;
+            lblAddressValue.Text = string.Empty;
         }
 
         private void btnExport_Click(object sender, EventArgs e)
         {
-            // TODO
+            MessageBox.Show("Not implemented", "WARNING", MessageBoxButtons.OK, MessageBoxIcon.Warning);
         }
 
-        private void btnSave_Click(object sender, EventArgs e)
+        private void btnClose_Click(object sender, EventArgs e)
         {
             Close();
         }
